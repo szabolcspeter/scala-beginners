a7249b11248ebf94e23f58fbc0c966ebab478b91
Szabolcs Peter <szabi.peter@gmail.com>
2025-11-10 09:58:51 +0100

    Values, Variables and Types update

--- src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -2,8 +2,14 @@ package lectures.part1basics
 
 object ValuesVariablesTypes extends App {
 
-  val x: Int = 42
+  // val x: Int = 42
+
+  // COMPILER CAN INFER TYPES
+  val x = 42
   println(x)
+
+  // VALS ARE IMMUTABLE
+  // x = 2
 
   val aString: String = "hello"
   val anotherString = "goodbye"
@@ -11,10 +17,14 @@ object ValuesVariablesTypes extends App {
   val aChar: Char = 'a'
   val anInt: Int = x
   val aShort: Short = 4613
-  val aLong: Long = 1234567891234L
+  val aLong: Long = 5273985273895237L
   val aFloat: Float = 2.0f
-  val aDouble: Double = 3.14d
+  val aDouble: Double = 3.14
 
+  // VARIABLES
   var aVariable: Int = 4
-  aVariable = 5
+  aVariable = 5 // side effects
+
+  // functional programming involves working less with variables, prefer vals over vars
+
 }
