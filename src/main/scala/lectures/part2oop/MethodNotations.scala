74f84599c3f8d45ceb7d72f9336fabefaaca7a56
Szabolcs Peter <szabi.peter@gmail.com>
2026-01-02 11:05:41 +0100

    MethodNotations

---
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -0,0 +1,44 @@
+package lectures.part2oop
+
+import scala.language.postfixOps
+
+object MethodNotations extends App {
+
+  class Person(val name: String, favoriteMovie: String) {
+
+    def likes(movie: String): Boolean = movie == favoriteMovie
+    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def unary_! : String = s"$name, what the heck?!"
+    def isAlive: Boolean = true
+    def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+  }
+
+  val mary = new Person("Mary", "Inception")
+  println(mary.likes("Inception"))
+  println(mary likes "Inception") // equivalent
+  // infix notation = operator notation (syntactic sugar)
+
+  // "operators" in Scala
+  val tom = new Person("Tom", "Fight Club")
+  println(mary + tom)
+  println(mary.+(tom))
+
+  // ALL OPERATORS ARE METHODS.
+  println(1 + 3)
+  println(1.+(3))
+
+  // prefix notation
+  val x = -1
+  val y = 1.unary_- // unary_ prefix only works with - + ~ !
+
+  println(!mary)
+  println(mary.unary_!)
+
+  // postfix notation
+  println(mary.isAlive)
+  println(mary isAlive)
+
+  // apply
+  println(mary.apply())
+  println(mary()) // equivalent
+}
