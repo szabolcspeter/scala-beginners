d68f824b036ae68ed28728a70b86bdd2191db021
Szabolcs Peter <szabi.peter@gmail.com>
2026-03-10 14:36:46 +0100

    MyList exercise

---
+++ src/main/scala/exercises/part2oop/MyList.scala
@@ -0,0 +1,38 @@
+package exercises.part2oop
+
+abstract class MyList {
+
+  def head: Int
+  def tail: MyList
+  def isEmpty: Boolean
+  def add(element: Int): MyList
+  def printElements: String
+  override def toString: String = s"[$printElements]"
+}
+
+object Empty extends MyList {
+  def head: Int = throw new NoSuchElementException
+  def tail: MyList = throw new NoSuchElementException
+  def isEmpty: Boolean = true
+  def add(element: Int): MyList = new Cons(element, Empty)
+  def printElements: String = ""
+}
+
+class Cons(h: Int, t: MyList) extends MyList {
+  def head: Int = h
+  def tail: MyList = t
+  def isEmpty: Boolean = false
+  def add(element: Int): MyList = new Cons(element, this)
+  def printElements: String =
+    if (t.isEmpty) "" + h
+    else s"$h ${t.printElements}"
+}
+
+object ListTest extends App {
+  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  println(list.tail.head)
+  println(list.add(4).head)
+  println(list.isEmpty)
+
+  print(list.toString)
+}
