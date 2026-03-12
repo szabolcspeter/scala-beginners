bf4e51851d2f969a647e9d3c498ea4d840063a14
Szabolcs Peter <szabi.peter@gmail.com>
2026-03-12 11:53:11 +0100

    AnonymousClasses

---
+++ src/main/scala/lectures/part2oop/AnonymousClasses.scala
@@ -0,0 +1,36 @@
+package lectures.part2oop
+
+object AnonymousClasses extends App {
+
+  abstract class Animal {
+    def eat(): Unit
+  }
+
+  // anonymous class
+  val funnyAnimal: Animal = new Animal {
+    override def eat(): Unit = println("ahahahahahahah")
+  }
+
+  /*
+      equivalent with
+
+      class AnonymousClasses$$anon$1 extends Animal {
+        override def eat: Unit = println("ahahahahahaah")
+      }
+      val funnyAnimal: Animal = new AnonymousClasses$$anon$1
+     */
+
+  println(funnyAnimal.getClass)
+
+
+  // anonymous classes work for non-abstract/traits types as well
+  class Person(name: String) {
+    def sayHi(): Unit = println(s"Hi, my name is $name, how can I help?")
+  }
+
+  val jim = new Person("Jim") {
+    override def sayHi(): Unit = println(s"Hi, my name is Jim, how can I be of service?")
+  }
+
+
+}
