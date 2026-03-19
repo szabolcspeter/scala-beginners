d0fbbf6d296977c973a444d70a06bfdff567fe35
Szabolcs Peter <szabi.peter@gmail.com>
2026-03-19 11:25:19 +0100

    WhatsAFunction

---
+++ src/main/scala/lectures/part3fp/WhatsAFunction.scala
@@ -0,0 +1,32 @@
+package lectures.part3fp
+
+object WhatsAFunction extends App {
+
+  // DREAM: use functions as first class elements
+  // problem: oop
+
+  val doubler = new MyFunction[Int, Int] {
+    override def apply(element: Int): Int = element * 2
+  }
+
+  println(doubler(2)) // doubler is an instance of function like class can be called like a function
+
+  trait MyFunction[A, B] {
+    def apply(element: A): B
+  }
+
+  // function types = Function1[A, B]
+  val stringToIntConverter = new Function1[String, Int] {
+    override def apply(string: String): Int = string.toInt
+  }
+
+  println(stringToIntConverter("3") + 4 )
+
+  // Function types Function2[A, B, R] === (A,B) => R
+  val adder: ((Int, Int) => Int) = new Function2[Int, Int, Int] {
+    override def apply(a: Int, b: Int): Int = a + b
+  }
+
+  // ALL SCALA FUNCTIONS ARE OBJECTS
+}
+
